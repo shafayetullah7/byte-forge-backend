0dba7f6822e43858b628f23c61ad4f149aaf7002
Shafayet Ullah <shafayet769@gmail.com>
2026-04-12 16:10:44 +0600

    resetted db

--- src/_db/drizzle/migrations/0049_adorable_serpent_society.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "is_active" boolean DEFAULT true NOT NULL;
--- src/_db/drizzle/migrations/0050_next_the_stranger.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "shop_verification" ADD COLUMN "rejection_reason" text;--> statement-breakpoint
-ALTER TABLE "shop_verification" ADD COLUMN "admin_notes" text;
